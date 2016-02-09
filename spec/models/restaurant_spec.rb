6a9671440c3b977056030a65ce1a736b2925dce8
Miles Stanfield <miles.stanfield@turner.com>
2016-02-09 12:09:25 -0500

    created restaurants has_many ratings association

--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -1,7 +1,15 @@
 require 'spec_helper'
 
 describe Restaurant do
+  subject(:restaurant){ Restaurant.new }
+
   describe 'attributes' do
     it_has_attributes 'name'
+  end
+
+  describe 'associations' do
+    it 'has many ratings' do
+      expect(restaurant.ratings).to eq []
+    end
   end
 end
