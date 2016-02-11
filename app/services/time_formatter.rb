670ee383f2b3d467ee7c1e82dc1af1642b257062
Miles Stanfield <miles.stanfield@turner.com>
2016-02-11 09:45:26 -0500

    work on styling cards on homepage

--- app/services/time_formatter.rb
+++ app/services/time_formatter.rb
@@ -1,5 +1,20 @@
 class TimeFormatter
-  def self.visit_time(time)
-    time.strftime('%m/%d/%Y')
+  class << self
+
+    def visit_time(time)
+      time.strftime(strf)
+    end
+
+    def days_ago(visited_time, control_time = nil)
+      control_time = control_time || Time.now
+      parsed_visit_time = Time.strptime(visited_time, strf)
+      (control_time - parsed_visit_time).to_i / 1.day
+    end
+
+    private
+
+    def strf
+      '%m/%d/%Y'
+    end
   end
 end
--- spec/services/time_formatter_spec.rb
+++ spec/services/time_formatter_spec.rb
@@ -6,4 +6,11 @@ describe TimeFormatter do
       expect(TimeFormatter.visit_time(now_time)).to eq '02/09/2016'
     end
   end
+
+  describe '.days_ago(formatted_time)' do
+    it 'returns days since time argument' do
+      control_time = Time.parse('2016-02-11 08:53:34 -0500')
+      expect(TimeFormatter.days_ago('02/05/2016', control_time)).to eq 6
+    end
+  end
 end
